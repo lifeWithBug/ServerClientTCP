2bb3da4373548dfb180a0ce4b3d69a845c5b84aa
18879125132 <18879125132@163.com>
2022-03-15 00:36:16 +0800

    添加项目文件。

---
+++ ClientTCP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClientTCP
+{
+    class Program
+    {
+        static void Main()
+        {
+            Socket clientSocket=new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.Connect(new IPEndPoint(IPAddress.Parse("192.168.0.100"), 8081));
+
+            byte[] clientBuf = new byte[1024];
+            int count = clientSocket.Receive(clientBuf);
+            string msgReceive=Encoding.UTF8.GetString(clientBuf,0,count);
+            Console.WriteLine("收到的消息为："+msgReceive);
+
+            while (true)
+            {
+                string msgSend = Console.ReadLine();
+                clientSocket.Send(Encoding.UTF8.GetBytes(msgSend));
+            }
+            
+
+            Console.ReadKey();
+            clientSocket.Close();
+        }
+    }
+}
---
+++ ServerTCP/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerTCP
+{
+    class Program
+    {
+        static void Main()
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(new IPEndPoint(IPAddress.Parse("192.168.0.100"), 8081));
+            serverSocket.Listen(10);
+
+            //向客户端发送消息
+            Socket clientSocket = serverSocket.Accept();
+            string MsgSend = "Hello,你好啊！";
+            byte[] clientBuf = Encoding.UTF8.GetBytes(MsgSend);
+            clientSocket.Send(clientBuf);
+
+            StartServerAsync(clientSocket);
+
+            Console.ReadKey();
+            clientSocket.Close();
+            serverSocket.Close();
+        }
+        static byte[] serverBuf = new byte[1024];
+        static void StartServerAsync(Socket clientSocket)
+        {
+            //接受客户端消息
+            
+            clientSocket.BeginReceive(serverBuf,0,1024,SocketFlags.None, ReceiveCallBack, clientSocket);
+            
+        }
+
+        static void ReceiveCallBack(IAsyncResult ar)
+        {
+            Socket clientSocket = ar.AsyncState as Socket;
+
+            int count = clientSocket.EndReceive(ar);
+            string MsgReceive = Encoding.UTF8.GetString(serverBuf, 0, count);
+            Console.WriteLine($"接收到的数据为：{MsgReceive}");
+            clientSocket.BeginReceive(serverBuf, 0, 1024, SocketFlags.None, ReceiveCallBack, clientSocket);
+        }
+    }
+}
